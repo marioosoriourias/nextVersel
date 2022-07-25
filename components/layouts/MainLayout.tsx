7e9f11d7e9cb1593b842cd4295ffab750f86ad69
Mario <mario.osoriourias@gmail.com>
2022-07-25 17:31:39 -0600

    deployment verse.

---
+++ components/layouts/MainLayout.tsx
@@ -0,0 +1,29 @@
+import Head from 'next/head';
+import { FC, ReactNode } from "react";
+import { Navbar } from '../Navbar';
+import styles from './MainLayout.module.css';
+
+type PropsWithChildren = {
+  children: ReactNode
+}
+
+export const MainLayout: FC<PropsWithChildren> = ({ children }) => {
+    return (
+        <div className={styles.container}>
+          
+          <Head>
+            <title>Home - Fernando</title>
+            <meta name="description" content="Home Page" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+        
+          <Navbar />
+    
+          <main className={styles.main}>
+    
+            { children }
+    
+          </main>     
+        </div>
+      )
+};
